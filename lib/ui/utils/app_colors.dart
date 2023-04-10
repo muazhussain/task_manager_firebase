9a85039613cc94f7e88cf3848756b6491bc1e2b4
Muaz <muaz4488@gmail.com>
2023-04-10 21:51:49 +0600

    AppColors class is added.

---
+++ lib/ui/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const mainColor = Color(0xFF202E59);
+  static const secondaryColor = Color(0xFF6C8EE3);
+  static const smallTextColor = Color(0xFF2E3253);
+  static const textHolder = Color(0xFFEDF0F8);
+  static const textGrey = Color(0xFFAFB1B5);
+}

423895204475028cd2418c79793c015850dd92ef
Muaz <muaz4488@gmail.com>
2023-04-10 21:44:47 +0600

    Initial Code
    Initial codebase for 'Task Manager' app.
    Assets are added.
    App icon and app name are updated.

---
+++ android/app/src/main/kotlin/com/muazhussain/tm/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.muazhussain.tm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const TaskManagerApp());
+}
+
+class TaskManagerApp extends StatelessWidget {
+  const TaskManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task Manager App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: null,
+    );
+  }
+}
